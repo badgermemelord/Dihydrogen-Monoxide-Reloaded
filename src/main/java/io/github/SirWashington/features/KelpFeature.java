bcf0aea70fdfa9ec28324b9f2f8b6cac96ee7d9d
Sir Washington <badgerthememelord@gmail.com>
2022-07-26 21:12:17 +0200

    use addwater for buckets
    buckets place extra water on top

---
+++ src/main/java/io/github/SirWashington/features/KelpFeature.java
@@ -0,0 +1,45 @@
+package io.github.SirWashington.features;
+
+import net.minecraft.block.BlockState;
+import net.minecraft.block.Blocks;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Direction;
+
+import java.util.ArrayList;
+
+import static io.github.SirWashington.FlowWater.world;
+
+public class KelpFeature {
+
+    public static void Execute(BlockPos kelpPos, BlockState kpBS) {
+
+        System.out.println("called");
+        int count = 0;
+        boolean nonFullFluidBlock = false;
+        int totalWaterLevel = 0;
+        int centerWaterLevel = 8;
+
+        ArrayList<BlockPos> blocks = new ArrayList<>(4);
+        for (Direction dir : Direction.Type.HORIZONTAL) {
+            blocks.add(kelpPos.offset(dir));
+
+            for (BlockPos block : blocks) {
+                BlockState internalBS = CachedWater.getBlockState(block);
+                if (internalBS.getBlock() == Blocks.WATER || internalBS.getBlock() == Blocks.AIR) {
+                    count += 1;
+                    int level = internalBS.getFluidState().getLevel();
+                    totalWaterLevel += level;
+                }
+                int level = CachedWater.getWaterLevel(block, false);
+                //System.out.println(level);
+                //System.out.println("tot " + totalWaterLevel);
+            }
+            if (totalWaterLevel <= (count - 1) * 8) {
+                nonFullFluidBlock = true;
+            }
+            if (nonFullFluidBlock) {
+                world.breakBlock(kelpPos, true);
+            }
+        }
+    }
+}
---
+++ src/main/java/io/github/SirWashington/mixin/BucketMixin.java
@@ -0,0 +1,79 @@
+package io.github.SirWashington.mixin;
+
+import io.github.SirWashington.features.BucketFeatures;
+import io.github.SirWashington.features.CachedWater;
+import net.minecraft.advancement.criterion.Criteria;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.FluidDrainable;
+import net.minecraft.block.FluidFillable;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.fluid.Fluids;
+import net.minecraft.item.BucketItem;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.ItemUsage;
+import net.minecraft.server.network.ServerPlayerEntity;
+import net.minecraft.stat.Stats;
+import net.minecraft.util.Hand;
+import net.minecraft.util.TypedActionResult;
+import net.minecraft.util.hit.BlockHitResult;
+import net.minecraft.util.hit.HitResult;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Direction;
+import net.minecraft.world.RaycastContext;
+import net.minecraft.world.World;
+import net.minecraft.world.event.GameEvent;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Overwrite;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.Redirect;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import net.minecraft.advancement.criterion.Criteria;
+import net.minecraft.block.Block;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.FluidDrainable;
+import net.minecraft.block.FluidFillable;
+import net.minecraft.block.Material;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.fluid.FlowableFluid;
+import net.minecraft.fluid.Fluid;
+import net.minecraft.fluid.Fluids;
+import net.minecraft.item.Item.Settings;
+import net.minecraft.particle.ParticleTypes;
+import net.minecraft.server.network.ServerPlayerEntity;
+import net.minecraft.sound.SoundCategory;
+import net.minecraft.sound.SoundEvent;
+import net.minecraft.sound.SoundEvents;
+import net.minecraft.stat.Stats;
+import net.minecraft.tag.FluidTags;
+import net.minecraft.util.Hand;
+import net.minecraft.util.TypedActionResult;
+import net.minecraft.util.hit.BlockHitResult;
+import net.minecraft.util.hit.HitResult.Type;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Direction;
+import net.minecraft.world.World;
+import net.minecraft.world.WorldAccess;
+import net.minecraft.world.RaycastContext.FluidHandling;
+import net.minecraft.world.event.GameEvent;
+import org.jetbrains.annotations.Nullable;
+
+@Mixin(net.minecraft.item.BucketItem.class)
+public abstract class BucketMixin{
+
+    @Redirect(
+            method = "placeFluid",
+            at = @At(
+                    value = "INVOKE",
+                    target = "Lnet/minecraft/world/World;setBlockState(Lnet/minecraft/util/math/BlockPos;Lnet/minecraft/block/BlockState;I)Z"
+            )
+    )
+    private boolean bucketPlace(World instance, BlockPos pos, BlockState state, int flags) {
+        boolean returnValue;
+        returnValue = BucketFeatures.addWater(8, pos, instance);
+        return returnValue;
+    }
+
+
+}
