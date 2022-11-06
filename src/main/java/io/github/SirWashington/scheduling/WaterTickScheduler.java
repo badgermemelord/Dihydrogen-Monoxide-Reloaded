4ee87fd8241eacb170fe053f5fe99300a177013d
Sir Washington <badgerthememelord@gmail.com>
2022-11-06 21:17:24 +0100

    laptop push

--- src/main/java/io/github/SirWashington/scheduling/WaterTickScheduler.java
+++ src/main/java/io/github/SirWashington/scheduling/WaterTickScheduler.java
@@ -2,10 +2,16 @@ package io.github.SirWashington.scheduling;
 
 import io.github.SirWashington.FlowWater;
 import io.github.SirWashington.features.CachedWater;
+import it.unimi.dsi.fastutil.longs.Long2LongArrayMap;
+import it.unimi.dsi.fastutil.longs.Long2LongOpenHashMap;
+import it.unimi.dsi.fastutil.longs.LongArrays;
 import net.minecraft.fluid.FluidState;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.ChunkPos;
 import net.minecraft.world.World;
+import net.minecraft.world.chunk.Chunk;
 import net.minecraft.world.chunk.ChunkStatus;
+import net.minecraft.world.chunk.EmptyChunk;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -19,10 +25,23 @@ public class WaterTickScheduler {
 
 
     public static List<Long> BlocksToTickNext = new ArrayList<>();
-
+    public static List<List<Long>> Chunks = new ArrayList<>();
+    public static Long2LongArrayMap Chunk2BlockMap = new Long2LongArrayMap();
     public static List<Long> BlocksToTick = new ArrayList<>();
 
-    public static void scheduleFluidBlock(BlockPos pos) {
+    public static void unloadChunk(ChunkPos chunkPos) {
+        Long posToUnload = chunkPos.toLong();
+        Chunk2BlockMap.remove(posToUnload);
+    }
+    public static void loadChunk(ChunkPos chunkPos, Long[] waterBlocksArray) {
+        Long posToLoad = chunkPos.toLong();
+        Chunk2BlockMap.put(posToLoad, waterBlocksArray);
+        Chunk2BlockMap.
+    }
+
+    public static void scheduleFluidBlock(BlockPos pos, ChunkPos chunkPos) {
+
+        Long chunkPosAsLong = chunkPos.toLong();
 
         if(!BlocksToTickNext.contains(pos.asLong())) {
             BlocksToTickNext.add(pos.asLong());
